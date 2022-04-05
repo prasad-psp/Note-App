db70a30ed6005381bc605c9290c44b1fc4f9db19
Admin <prasad.parshram123@gmail.com>
2022-04-05 13:09:48 +0530

    added paging & implement repo in HomeFragViewModel.java

--- app/src/main/java/com/psp/note_app/viewmodel/HomeFragViewModel.java
+++ app/src/main/java/com/psp/note_app/viewmodel/HomeFragViewModel.java
@@ -1,24 +1,44 @@
 package com.psp.note_app.viewmodel;
 
+import android.content.Context;
+
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.paging.Pager;
 import androidx.paging.PagingConfig;
 import androidx.paging.PagingData;
+import androidx.paging.PagingLiveData;
 
 import com.psp.note_app.model.Note;
+import com.psp.note_app.repo.HomeFragRepo;
 
 public class HomeFragViewModel extends ViewModel {
 
-    private MutableLiveData<PagingData<Note>> notesLiveData = new MutableLiveData<>();
+    private final MutableLiveData<PagingData<Note>> notesLiveData = new MutableLiveData<>();
 
-    private PagingConfig pagingConfig = new PagingConfig(
+    private final PagingConfig pagingConfig = new PagingConfig(
             20,
             20,
             false,
             20
         );
 
-    public void init() {
+    private HomeFragRepo repo; // Repo object
 
+    public void init(Context context, LifecycleOwner lifecycleOwner) {
+        // init repo
+        repo = new HomeFragRepo(context);
+        // init paging livedata
+        PagingLiveData.cachedIn(
+                PagingLiveData.getLiveData(new Pager<>(pagingConfig, () -> repo.getNotes())),
+                        lifecycleOwner.getLifecycle()
+        ).observe(lifecycleOwner, notesLiveData::setValue);
+    }
+
+
+    public LiveData<PagingData<Note>> getNotesObserver() {
+        return notesLiveData;
     }
 }
