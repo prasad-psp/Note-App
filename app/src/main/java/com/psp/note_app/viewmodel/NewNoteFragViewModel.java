64ed7066ed7337867b01298ebe1bcb46fae80fe5
Admin <prasad.parshram123@gmail.com>
2022-04-05 13:18:48 +0530

    implement repo in NewNoteFragViewModel.java

--- app/src/main/java/com/psp/note_app/viewmodel/NewNoteFragViewModel.java
+++ app/src/main/java/com/psp/note_app/viewmodel/NewNoteFragViewModel.java
@@ -1,7 +1,28 @@
 package com.psp.note_app.viewmodel;
 
+import android.content.Context;
 import androidx.lifecycle.ViewModel;
+
+import com.psp.note_app.model.Note;
+import com.psp.note_app.repo.NewNoteFragRepo;
+import com.psp.note_app.utils.DateUtils;
 
 public class NewNoteFragViewModel extends ViewModel {
 
+    private final NewNoteFragRepo repo; // Repo object
+
+    public NewNoteFragViewModel(Context context) {
+        // init repo
+        repo = new NewNoteFragRepo(context);
+    }
+
+    public void addNote(String title, String desc) {
+        Note note = new Note();
+        note.setId(0);
+        note.setTitle(title);
+        note.setDesc(desc);
+        note.setDate(DateUtils.getCurrDate());
+        note.setTime(DateUtils.getCurrTime());
+        repo.addNote(note);
+    }
 }
