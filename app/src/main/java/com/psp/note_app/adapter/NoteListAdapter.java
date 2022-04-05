dfb7dff08a3ad05b8cb6a186eb7614f1684333fa
Admin <prasad.parshram123@gmail.com>
2022-04-05 17:50:34 +0530

    added note paging adapter

--- app/src/main/java/com/psp/note_app/adapter/NoteListAdapter.java
+++ app/src/main/java/com/psp/note_app/adapter/NoteListAdapter.java
@@ -1,4 +1,72 @@
 package com.psp.note_app.adapter;
 
-public class NoteListAdapter {
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.paging.PagingDataAdapter;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.psp.note_app.databinding.ItemNoteRecyclerviewBinding;
+import com.psp.note_app.model.Note;
+
+public class NoteListAdapter extends PagingDataAdapter<Note, NoteListAdapter.MyViewHolder> {
+    private final OnItemClickListener itemClickListener;
+
+    public NoteListAdapter(OnItemClickListener itemClickListener) {
+        super(callback);
+        this.itemClickListener = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemNoteRecyclerviewBinding binding = ItemNoteRecyclerviewBinding
+                .inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new MyViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Note note = getItem(position);
+
+        if(note != null) {
+            holder.binding.txtTitle.setText(note.getTitle());
+            String dateTime = note.getDate()+" "+note.getTime();
+            holder.binding.txtDateTime.setText(dateTime);
+        }
+
+        holder.binding.rootView.setOnClickListener(view -> {
+            if(note != null) {
+                itemClickListener.onItemClicked(note);
+            }
+        });
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        private final ItemNoteRecyclerviewBinding binding;
+        public MyViewHolder(ItemNoteRecyclerviewBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    static DiffUtil.ItemCallback<Note> callback = new DiffUtil.ItemCallback<Note>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @SuppressLint("DiffUtilEquals")
+        @Override
+        public boolean areContentsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem == newItem;
+        }
+    };
+
+    interface OnItemClickListener {
+        void onItemClicked(Note note);
+    }
 }
