a06f3c7f0e931dbc831e42c9c656de5ebe59ce1d
Admin <prasad.parshram123@gmail.com>
2022-04-06 20:14:40 +0530

    added new constants in Constants.java

--- app/src/main/java/com/psp/note_app/utils/Constants.java
+++ app/src/main/java/com/psp/note_app/utils/Constants.java
@@ -5,4 +5,8 @@ public class Constants {
     public static final String DATABASE_NAME = "note_db";
     public static final String TABLE_NAME = "note";
 
+    public static final String TITLE_KEY = "title_key";
+    public static final String DESC_KEY = "desc_key";
+    public static final String ID_KEY = "id_key";
+
 }
