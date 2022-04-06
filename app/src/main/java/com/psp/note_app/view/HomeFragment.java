c606ed02a00cc95ee3d70ca65c75a86b3fd4e341
Admin <prasad.parshram123@gmail.com>
2022-04-06 20:16:54 +0530

    implement navigation

--- app/src/main/java/com/psp/note_app/view/HomeFragment.java
+++ app/src/main/java/com/psp/note_app/view/HomeFragment.java
@@ -1,5 +1,9 @@
 package com.psp.note_app.view;
 
+import static com.psp.note_app.utils.Constants.DESC_KEY;
+import static com.psp.note_app.utils.Constants.ID_KEY;
+import static com.psp.note_app.utils.Constants.TITLE_KEY;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,9 +11,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.paging.PagingData;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +23,6 @@ import com.psp.note_app.R;
 import com.psp.note_app.adapter.NoteListAdapter;
 import com.psp.note_app.databinding.FragmentHomeBinding;
 import com.psp.note_app.model.Note;
-import com.psp.note_app.repo.NewNoteFragRepo;
-import com.psp.note_app.utils.DateUtils;
 import com.psp.note_app.viewmodel.HomeFragViewModel;
 
 
@@ -61,17 +63,16 @@ public class HomeFragment extends Fragment implements NoteListAdapter.OnItemClic
         });
 
         binding.btnFloating.setOnClickListener(view1 -> {
-            Toast.makeText(view1.getContext(), "Floating button clicked", Toast.LENGTH_SHORT).show();
-            logmsg("Floating button clicked");
+            Navigation.findNavController(view1).navigate(R.id.action_homeFragment_to_newNoteFragment);
         });
     }
 
     @Override
-    public void onItemClicked(Note note) {
-        Toast.makeText(this.requireContext(), "item clicked "+note.getTime(), Toast.LENGTH_SHORT).show();
-    }
-
-    private void logmsg(String msg) {
-        Log.d("HomeFragment",msg);
+    public void onItemClicked(View view, Note note) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(ID_KEY, note.getId());
+        bundle.putString(TITLE_KEY, note.getTitle());
+        bundle.putString(DESC_KEY, note.getDesc());
+        Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_newNoteFragment,bundle);
     }
 }
