fae08d2d17d4f911f68eaf0df917f2afd9703cbf
Admin <prasad.parshram123@gmail.com>
2022-04-11 19:28:44 +0530

    added new View model factory in viewmodel package

---
+++ app/src/main/java/com/psp/note_app/viewmodel/AppViewModelFactory.java
@@ -0,0 +1,44 @@
+package com.psp.note_app.viewmodel;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class AppViewModelFactory implements ViewModelProvider.Factory {
+
+    private final Context context;
+
+    private LifecycleOwner lifecycleOwner;
+
+    public AppViewModelFactory(Context context) {
+        this.context = context;
+    }
+
+    public AppViewModelFactory(Context context, LifecycleOwner lifecycleOwner) {
+        this.context = context;
+        this.lifecycleOwner = lifecycleOwner;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if(modelClass == MainViewModel.class) {
+            MainViewModel mainViewModel = new MainViewModel(context);
+            return (T) mainViewModel;
+        }
+        else if(modelClass == HomeFragViewModel.class) {
+            HomeFragViewModel homeFragViewModel = new HomeFragViewModel(context,lifecycleOwner);
+            return (T) homeFragViewModel;
+        }
+        else if(modelClass == NewNoteFragViewModel.class) {
+            NewNoteFragViewModel newNoteFragViewModel = new NewNoteFragViewModel(context);
+            return (T) newNoteFragViewModel;
+        }
+        else {
+            return null;
+        }
+    }
+}
